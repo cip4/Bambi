1e721676a2d5a8f0c5d26deb4b01f02a5f82253e
Stefan Meissner <s.meissner@flyeralarm.de>
2009-10-22 16:22:35 +0000

    git-svn-id: http://svn.cip4.org/svnroot/trunk@4221 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/java/org/cip4/bambi/richclient/web/DevicesBean.java
+++ src/java/org/cip4/bambi/richclient/web/DevicesBean.java
@@ -377,4 +377,12 @@ public class DevicesBean {
 			put2SelectedDeviceQueue(entry);
 		}
 	}
+
+	/**
+	 * Returns devices Url
+	 * @return Device Url
+	 */
+	public String getPrinter() {
+		return "<object width=\"480\" height=\"385\"><param name=\"movie\" value=\"http://www.youtube.com/v/JCcqyu9hb1k&hl=en&fs=1&rel=0&color1=0x006699&color2=0x54abd6\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"http://www.youtube.com/v/JCcqyu9hb1k&hl=en&fs=1&rel=0&color1=0x006699&color2=0x54abd6\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"480\" height=\"385\"></embed></object>";
+	}
 }
