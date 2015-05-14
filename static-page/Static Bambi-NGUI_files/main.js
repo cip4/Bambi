52ff1974b76740a4faa208d5df68e6e38e77c5ca
Alex Khilov <khilov@gmail.com>
2015-05-14 19:23:21 +0300

    Add Bambi-NGUI sample page as static page

---
+++ static-page/Static Bambi-NGUI_files/main.js
@@ -0,0 +1,44 @@
+application = {
+	bindAll : function() {
+		$(".button.details").bind('click', function(e) {
+			$(this).parent().siblings(".device-details").toggleClass("hide");
+		});
+		
+		$(".button.entries").bind('click', function(e) {
+			$(this).parent().parent().siblings(".main-panel").children(".queue-entries").toggleClass("hide");
+		});
+		
+		console.log("all ok: page loaded, scripts fired");
+		console.log("window.location: " + window.location);
+		console.log("location.hostname: " + location.hostname);
+		console.log("location.port: " + location.port);
+		
+		
+		var ws = new WebSocket("ws://" + location.hostname + ":" + location.port + "/SimWorker/echo");
+//		var ws = new WebSocket("ws://" + location.hostname + ":" + location.port + "/bambi-ngui/echo");
+		ws.onopen = function () {
+			console.log('ws onopen');
+			ws.send("ky-ky! это я - такое сообщение!");
+		};
+		ws.onclose = function () {
+			console.log('ws onclose');
+		};
+		ws.onerror = function (e) {
+			console.log('ws error', e);
+		};
+		ws.onmessage = function (e) {
+//			console.log('ws onmessage, full: ', e);
+			console.log('ws onmessage, data: ', e.data);
+			
+			var data = e.data;
+			var obj = JSON.parse(data);
+			console.log('obj: ', obj);
+			
+			$("#" + obj.deviceId + " .queueAll").text(obj.queueAll);
+		};
+	}
+};
+
+$(document).ready(function() {
+	application.bindAll();
+});
