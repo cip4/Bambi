9873b413fad1cfee48781447e394febb047e6332
Rainer Prosi <rainer.prosi@heidelberg.com>
2015-06-26 13:51:47 +0200

    added getQuery to MsgSubscription

--- bambi-war/src/main/java/org/cip4/bambi/core/messaging/MsgSubscription.java
+++ bambi-war/src/main/java/org/cip4/bambi/core/messaging/MsgSubscription.java
@@ -2,7 +2,7 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2014 The International Cooperation for the Integration of 
+ * Copyright (c) 2001-2015 The International Cooperation for the Integration of 
  * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
  * reserved.
  *
@@ -551,5 +551,14 @@ public class MsgSubscription implements Cloneable
 			return true;
 		}
 		return msgType.equals(theMessage.getType());
+	}
+
+	/**
+	 * 
+	 * @return the query message
+	 */
+	public JDFMessage getQuery()
+	{
+		return theMessage;
 	}
 }
