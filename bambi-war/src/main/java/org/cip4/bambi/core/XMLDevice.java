f453117ffc6d1d5635a1bd971d74ada7a07ffbca
Rainer Prosi <rainer.prosi@heidelberg.com>
2016-03-16 11:12:51 +0100

    avoid npe

--- bambi-war/src/main/java/org/cip4/bambi/core/XMLDevice.java
+++ bambi-war/src/main/java/org/cip4/bambi/core/XMLDevice.java
@@ -1,3 +1,72 @@
+/*
+ * The CIP4 Software License, Version 1.0
+ *
+ *
+ * Copyright (c) 2001-2016 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        The International Cooperation for the Integration of
+ *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
+ *    Processes in  Prepress, Press and Postpress" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact info@cip4.org.
+ *
+ * 5. Products derived from this software may not be called "CIP4",
+ *    nor may "CIP4" appear in their name, without prior written
+ *    permission of the CIP4 organization
+ *
+ * Usage of this software in commercial products is subject to restrictions. For
+ * details please consult info@cip4.org.
+  *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR
+ * THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the The International Cooperation for the Integration
+ * of Processes in Prepress, Press and Postpress and was
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
+ * Integration of Processes in  Prepress, Press and Postpress , please see
+ * <http://www.cip4.org/>.
+ *
+ *
+ */
 package org.cip4.bambi.core;
 
 import java.io.File;
@@ -90,20 +159,23 @@ public class XMLDevice extends XMLDoc
 	{
 		if (parentDevice._theQueueProcessor == null)
 		{
-			log.error("device with null queueprocessor - bailing ot: ID=" + parentDevice.getDeviceID());
+			log.error("device with null queueprocessor - bailing out: ID=" + parentDevice.getDeviceID());
 		}
-		final JDFQueue jdfQueue = parentDevice._theQueueProcessor.getQueue();
-		final EnumQueueStatus queueStatus = jdfQueue == null ? null : jdfQueue.getQueueStatus();
-		final int running = jdfQueue == null ? 0 : jdfQueue.numEntries(EnumQueueEntryStatus.Running);
-		final int waiting = jdfQueue == null ? 0 : jdfQueue.numEntries(EnumQueueEntryStatus.Waiting) + jdfQueue.numEntries(EnumQueueEntryStatus.Suspended);
-		final int completed = jdfQueue == null ? 0 : jdfQueue.numEntries(EnumQueueEntryStatus.Completed) + jdfQueue.numEntries(EnumQueueEntryStatus.Aborted);
-		final int all = jdfQueue == null ? 0 : jdfQueue.numEntries(null);
+		else
+		{
+			final JDFQueue jdfQueue = parentDevice._theQueueProcessor.getQueue();
+			final EnumQueueStatus queueStatus = jdfQueue == null ? null : jdfQueue.getQueueStatus();
+			final int running = jdfQueue == null ? 0 : jdfQueue.numEntries(EnumQueueEntryStatus.Running);
+			final int waiting = jdfQueue == null ? 0 : jdfQueue.numEntries(EnumQueueEntryStatus.Waiting) + jdfQueue.numEntries(EnumQueueEntryStatus.Suspended);
+			final int completed = jdfQueue == null ? 0 : jdfQueue.numEntries(EnumQueueEntryStatus.Completed) + jdfQueue.numEntries(EnumQueueEntryStatus.Aborted);
+			final int all = jdfQueue == null ? 0 : jdfQueue.numEntries(null);
 
-		deviceRoot.setAttribute("QueueStatus", queueStatus == null ? "Unknown" : queueStatus.getName());
-		deviceRoot.setAttribute("QueueWaiting", waiting, null);
-		deviceRoot.setAttribute("QueueRunning", running, null);
-		deviceRoot.setAttribute("QueueCompleted", completed, null);
-		deviceRoot.setAttribute("QueueAll", all, null);
+			deviceRoot.setAttribute("QueueStatus", queueStatus == null ? "Unknown" : queueStatus.getName());
+			deviceRoot.setAttribute("QueueWaiting", waiting, null);
+			deviceRoot.setAttribute("QueueRunning", running, null);
+			deviceRoot.setAttribute("QueueCompleted", completed, null);
+			deviceRoot.setAttribute("QueueAll", all, null);
+		}
 	}
 
 	/**
@@ -114,14 +186,21 @@ public class XMLDevice extends XMLDoc
 		// nop
 	}
 
+	/**
+	 * 
+	 */
 	private void addProcessors()
 	{
-		for (int i = 0; i < parentDevice._deviceProcessors.size(); i++)
+		for (AbstractDeviceProcessor proc : parentDevice._deviceProcessors)
 		{
-			parentDevice._deviceProcessors.get(i).addToDisplayXML(getRoot());
+			proc.addToDisplayXML(getRoot());
 		}
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	protected AbstractDevice getParentDevice()
 	{
 		return parentDevice;
