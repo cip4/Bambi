03fc5cac097c77500571c68cea7ce7e23ba2ba61
Rainer Prosi <rainer.prosi@heidelberg.com>
2017-02-23 14:43:23 +0100

    better rc evaluation for hot folders

--- bambi-war/src/main/java/org/cip4/bambi/core/DeviceHFListener.java
+++ bambi-war/src/main/java/org/cip4/bambi/core/DeviceHFListener.java
@@ -2,8 +2,8 @@
  *
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2015 The International Cooperation for the Integration of 
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * Copyright (c) 2001-2017 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -11,7 +11,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -19,17 +19,17 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
- *        The International Cooperation for the Integration of 
+ *        The International Cooperation for the Integration of
  *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
  *    Processes in  Prepress, Press and Postpress" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact info@cip4.org.
  *
  * 5. Products derived from this software may not be called "CIP4",
@@ -55,20 +55,20 @@
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration 
+ * individuals on behalf of the The International Cooperation for the Integration
  * of Processes in Prepress, Press and Postpress and was
- * originally based on software 
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
- * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
- *  
- * For more information on The International Cooperation for the 
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
  * Integration of Processes in  Prepress, Press and Postpress , please see
  * <http://www.cip4.org/>.
- *  
- * 
+ *
+ *
  */
 /**
- * 
+ *
  */
 package org.cip4.bambi.core;
 
@@ -80,17 +80,17 @@ import org.cip4.jdflib.util.hotfolder.QueueHotFolderListener;
 
 /**
  * @author Rainer Prosi, Heidelberger Druckmaschinen
- * 
+ *
  */
 public class DeviceHFListener extends BambiLogFactory implements QueueHotFolderListener
 {
 	/**
-	 * 
+	 *
 	 */
 	private final AbstractDevice device;
 
 	/**
-	 * 
+	 *
 	 * @param dev
 	 */
 	public DeviceHFListener(final AbstractDevice dev)
@@ -116,7 +116,13 @@ public class DeviceHFListener extends BambiLogFactory implements QueueHotFolderL
 
 		final JMFHandler jmfHandler = device.getJMFHandler(null);
 		final JDFResponse resp = clone.appendResponse(m.getEnumType());
-		return jmfHandler.handleMessage(m, resp);
+		final boolean handled = jmfHandler == null ? false : jmfHandler.handleMessage(m, resp);
+		boolean ok = handled && resp != null && resp.getReturnCode() == 0;
+		if (!ok)
+		{
+			log.warn("Problems with hot file");
+		}
+		return ok;
 	}
 
 	/**
@@ -125,6 +131,6 @@ public class DeviceHFListener extends BambiLogFactory implements QueueHotFolderL
 	@Override
 	public String toString()
 	{
-		return "DeviceHFListener [device=" + device + "]";
+		return "DeviceHFListener [deviceID=" + device == null ? null : device.getDeviceID() + "]";
 	}
 }
