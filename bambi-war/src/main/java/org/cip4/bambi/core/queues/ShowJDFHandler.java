f9d7340e66ec9027fef5e414f4ef3a38b393fe0d
Rainer Prosi <rainer.prosi@heidelberg.com>
2017-03-07 10:53:42 +0100

    add hook to processRaw

--- bambi-war/src/main/java/org/cip4/bambi/core/queues/ShowJDFHandler.java
+++ bambi-war/src/main/java/org/cip4/bambi/core/queues/ShowJDFHandler.java
@@ -3,8 +3,8 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2015 The International Cooperation for the Integration of 
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * Copyright (c) 2001-2017 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,7 +12,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -20,17 +20,17 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
- *        The International Cooperation for the Integration of 
+ *        The International Cooperation for the Integration of
  *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
  *    Processes in  Prepress, Press and Postpress" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact info@cip4.org.
  *
  * 5. Products derived from this software may not be called "CIP4",
@@ -56,17 +56,17 @@
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration 
+ * individuals on behalf of the The International Cooperation for the Integration
  * of Processes in Prepress, Press and Postpress and was
- * originally based on software 
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
- * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
- *  
- * For more information on The International Cooperation for the 
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
  * Integration of Processes in  Prepress, Press and Postpress , please see
  * <http://www.cip4.org/>.
- *  
- * 
+ *
+ *
  */
 package org.cip4.bambi.core.queues;
 
@@ -94,7 +94,7 @@ import org.cip4.jdflib.util.UrlUtil;
 
 /**
  * @author Dr. Rainer Prosi, Heidelberger Druckmaschinen AG
- * 
+ *
  * 13.01.2009
  */
 public class ShowJDFHandler extends ShowHandler
@@ -169,14 +169,7 @@ public class ShowJDFHandler extends ShowHandler
 		}
 		else if (is != null)
 		{
-			try
-			{
-				IOUtils.copy(is, r.getOutputStream());
-			}
-			catch (IOException e)
-			{
-				log.error("Cannot copy to response:", e);
-			}
+			processRaw(r, is);
 		}
 
 		final boolean bJDF = request.getBooleanParam(QueueProcessor.isJDF);
@@ -184,9 +177,21 @@ public class ShowJDFHandler extends ShowHandler
 		return r;
 	}
 
+	protected void processRaw(XMLResponse r, InputStream is)
+	{
+		try
+		{
+			IOUtils.copy(is, r.getOutputStream());
+		}
+		catch (IOException e)
+		{
+			log.error("Cannot copy to response:", e);
+		}
+	}
+
 	/**
 	 * copy all move all partitioned common nodeinfo status values into the root
-	 * 
+	 *
 	 * @param doc
 	 * @param request
 	 * @return
@@ -219,7 +224,7 @@ public class ShowJDFHandler extends ShowHandler
 	}
 
 	/**
-	 * 
+	 *
 	 * update the activation based on the request parameter
 	 * this is needed in case we serve the JDF of a requestqueueentry via http
 	 * @param request
