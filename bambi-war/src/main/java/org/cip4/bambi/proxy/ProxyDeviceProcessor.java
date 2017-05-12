e8eeeb6914e98e5fc70005507e42169523531c15
Rainer Prosi <rainer.prosi@heidelberg.com>
2017-05-12 12:38:26 +0200

    remove npe for proxy processor

--- bambi-war/src/main/java/org/cip4/bambi/proxy/ProxyDeviceProcessor.java
+++ bambi-war/src/main/java/org/cip4/bambi/proxy/ProxyDeviceProcessor.java
@@ -3,8 +3,8 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2015 The International Cooperation for the Integration of 
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * Copyright (c) 2001-2017 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,7 +12,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -20,17 +20,17 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
- *        The International Cooperation for the Integration of 
+ *        The International Cooperation for the Integration of
  *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
  *    Processes in  Prepress, Press and Postpress" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact info@cip4.org.
  *
  * 5. Products derived from this software may not be called "CIP4",
@@ -56,17 +56,17 @@
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration 
+ * individuals on behalf of the The International Cooperation for the Integration
  * of Processes in Prepress, Press and Postpress and was
- * originally based on software 
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
- * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
- *  
- * For more information on The International Cooperation for the 
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
  * Integration of Processes in  Prepress, Press and Postpress , please see
  * <http://www.cip4.org/>.
- *  
- * 
+ *
+ *
  */
 
 package org.cip4.bambi.proxy;
@@ -117,7 +117,7 @@ import org.cip4.jdflib.util.UrlUtil;
 
 /**
  * @author Dr. Rainer Prosi, Heidelberger Druckmaschinen AG
- * 
+ *
  * 16.10.2008
  */
 public class ProxyDeviceProcessor extends AbstractProxyProcessor
@@ -288,7 +288,7 @@ public class ProxyDeviceProcessor extends AbstractProxyProcessor
 
 		/**
 		 * @param n
-		 * @param resp 
+		 * @param resp
 		 * @return true if handled
 		 */
 		private boolean handleEvent(final JDFNotification n, final JDFResponse resp)
@@ -313,7 +313,7 @@ public class ProxyDeviceProcessor extends AbstractProxyProcessor
 	protected class NotificationQueryHandler extends NotificationHandler
 	{
 		/**
-		 * 
+		 *
 		 */
 		public NotificationQueryHandler()
 		{
@@ -371,7 +371,7 @@ public class ProxyDeviceProcessor extends AbstractProxyProcessor
 
 		/**
 		 * @param info
-		 * @return 
+		 * @return
 		 */
 		private boolean handleResourceInfo(final JDFResourceInfo info)
 		{
@@ -560,7 +560,7 @@ public class ProxyDeviceProcessor extends AbstractProxyProcessor
 	}
 
 	/**
-	 * 
+	 *
 	 * @see org.cip4.bambi.proxy.AbstractProxyProcessor#submitted(java.lang.String, org.cip4.jdflib.auto.JDFAutoQueueEntry.EnumQueueEntryStatus, java.lang.String, java.lang.String)
 	 * @param devQEID
 	 * @param newStatus
@@ -607,7 +607,14 @@ public class ProxyDeviceProcessor extends AbstractProxyProcessor
 
 		super.init(queueProcessor, statusListener, devProperties);
 		final JDFQueueEntry qe = getQueueEntry();
-		log.info("processQueueEntry queuentryID=" + qe.getQueueEntryID());
+		if (qe != null)
+		{
+			log.info("processQueueEntry queuentryID=" + qe.getQueueEntryID());
+		}
+		else
+		{
+			log.warn("processQueueEntry with missing queueEntry");
+		}
 	}
 
 	/**
