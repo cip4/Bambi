1b83f70dc83396eaa88327fd789b6436323ddddd
CEU\PROSIRAI <rainer.prosi@heidelberg.com>
2022-06-16 11:45:35 +0200

    missed test synchronization

--- src/test/java/org/cip4/bambi/core/SignalDispatcherTest.java
+++ src/test/java/org/cip4/bambi/core/SignalDispatcherTest.java
@@ -3,7 +3,7 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2021 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
+ * Copyright (c) 2001-2022 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
@@ -85,6 +85,7 @@ public class SignalDispatcherTest extends BambiTestCase
 		final JMFHandler h = new JMFHandler(rootDev);
 		dispatcher = new SignalDispatcher(rootDev);
 		dispatcher.addHandlers(h);
+		super.setUp();
 	}
 
 	/**
