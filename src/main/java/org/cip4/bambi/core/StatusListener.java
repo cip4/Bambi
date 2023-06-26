b4a2eeebdbdbd066734fa765c22553f0be171f65
Rainer Prosi <rainer.prosi@heidelberg.com>
2023-06-26 19:51:37 +0200

    removed some logging

--- src/main/java/org/cip4/bambi/core/StatusListener.java
+++ src/main/java/org/cip4/bambi/core/StatusListener.java
@@ -2,7 +2,7 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2020 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
+ * Copyright (c) 2001-2023 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
@@ -286,7 +286,6 @@ public class StatusListener extends BambiLogFactory implements IPersistable
 
 		if (!KElement.isWildCard(oldQEID))
 		{
-			log.info("removing subscription for: " + oldQEID);
 			dispatcher.removeSubScriptions(oldQEID, "*", null);
 		}
 
