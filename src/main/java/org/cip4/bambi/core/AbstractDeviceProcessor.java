e39ae388ccb3da214a3d370a20422894e449aaf5
Rainer Prosi <rainer.prosi@heidelberg.com>
2024-11-12 18:05:44 +0100

    cleanup

--- src/main/java/org/cip4/bambi/core/AbstractDeviceProcessor.java
+++ src/main/java/org/cip4/bambi/core/AbstractDeviceProcessor.java
@@ -805,7 +805,6 @@ public abstract class AbstractDeviceProcessor extends BambiLogFactory implements
 	{
 		_parent = device;
 		_queueProcessor = device.getQueueProcessor();
-		// _statusListener = device.getStatusListener();
 		_statusListener.setDispatcher(device.getSignalDispatcher());
 		_statusListener.setDeviceID(device.getDeviceID());
 		_statusListener.setIcsVersions(device.getICSVersions());
