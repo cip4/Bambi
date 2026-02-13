8ac0e0a027948179b0315072c4cf3d6ed13acf1e
Rainer Prosi <rainer.prosi@heidelberg.com>
2026-02-13 11:12:47 +0100

    more test fix fixes

--- src/test/java/org/cip4/bambi/core/DataRequestHandlerTest.java
+++ src/test/java/org/cip4/bambi/core/DataRequestHandlerTest.java
@@ -1,8 +1,8 @@
 /**
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2024 The International Cooperation for the Integration of 
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * Copyright (c) 2001-2026 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -18,17 +18,17 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
- *        The International Cooperation for the Integration of 
+ *        The International Cooperation for the Integration of
  *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
  *    Processes in  Prepress, Press and Postpress" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact info@cip4.org.
  *
  * 5. Products derived from this software may not be called "CIP4",
@@ -54,17 +54,17 @@
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration 
+ * individuals on behalf of the The International Cooperation for the Integration
  * of Processes in Prepress, Press and Postpress and was
- * originally based on software 
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
- * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
- *  
- * For more information on The International Cooperation for the 
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
  * Integration of Processes in  Prepress, Press and Postpress , please see
  * <http://www.cip4.org/>.
- *  
- * 
+ *
+ *
  */
 package org.cip4.bambi.core;
 
@@ -84,7 +84,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 /**
- * 
  * @author rainer prosi
  * @date November 4, 2010
  */
@@ -106,8 +105,6 @@ public class DataRequestHandlerTest extends BambiTestCase
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test
@@ -136,8 +133,6 @@ public class DataRequestHandlerTest extends BambiTestCase
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test
@@ -153,8 +148,6 @@ public class DataRequestHandlerTest extends BambiTestCase
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test
@@ -169,8 +162,6 @@ public class DataRequestHandlerTest extends BambiTestCase
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test
@@ -185,8 +176,6 @@ public class DataRequestHandlerTest extends BambiTestCase
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test
@@ -197,13 +186,12 @@ public class DataRequestHandlerTest extends BambiTestCase
 		final ContainerRequest request = new ContainerRequest();
 		request.setRequestURI("http://dev/blub/data/a%20b.pdf");
 
-		new File(sm_dirTestDataTemp + "ID_42/JDF/a b.pdf").createNewFile();
+		final File file = new File(sm_dirTestDataTemp + "ID_42/JDF/a b.pdf");
+		FileUtil.createNewFile(file);
 		assertNotNull(rh.handleGet(request));
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test(expected = IllegalArgumentException.class)
@@ -219,8 +207,6 @@ public class DataRequestHandlerTest extends BambiTestCase
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test(expected = IllegalArgumentException.class)
@@ -236,8 +222,6 @@ public class DataRequestHandlerTest extends BambiTestCase
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test(expected = IllegalArgumentException.class)
@@ -249,13 +233,13 @@ public class DataRequestHandlerTest extends BambiTestCase
 
 		rh.handleGet(request);
 		if (!PlatformUtil.isWindows())
+		{
 			throw new IllegalArgumentException("works on linux");
+		}
 
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test(expected = IllegalArgumentException.class)
@@ -267,12 +251,12 @@ public class DataRequestHandlerTest extends BambiTestCase
 
 		rh.handleGet(request);
 		if (!PlatformUtil.isWindows())
+		{
 			throw new IllegalArgumentException("works on linux");
+		}
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test
@@ -293,8 +277,6 @@ public class DataRequestHandlerTest extends BambiTestCase
 	}
 
 	/**
-	 * 
-	 * 
 	 * @throws Exception its a test!
 	 */
 	@Test
--- src/test/java/org/cip4/bambi/workers/sim/JobLoaderTest.java
+++ src/test/java/org/cip4/bambi/workers/sim/JobLoaderTest.java
@@ -3,8 +3,8 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2021 The International Cooperation for the Integration of 
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * Copyright (c) 2001-2026 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,7 +12,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -20,17 +20,17 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
- *        The International Cooperation for the Integration of 
+ *        The International Cooperation for the Integration of
  *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
  *    Processes in  Prepress, Press and Postpress" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact info@cip4.org.
  *
  * 5. Products derived from this software may not be called "CIP4",
@@ -56,17 +56,17 @@
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration 
+ * individuals on behalf of the The International Cooperation for the Integration
  * of Processes in Prepress, Press and Postpress and was
- * originally based on software 
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
- * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
- *  
- * For more information on The International Cooperation for the 
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
  * Integration of Processes in  Prepress, Press and Postpress , please see
  * <http://www.cip4.org/>.
- *  
- * 
+ *
+ *
  */
 package org.cip4.bambi.workers.sim;
 
@@ -89,8 +89,8 @@ public class JobLoaderTest extends BambiTestCaseBase
 	{
 		final BambiTestDevice rootDev = new BambiTestDevice();
 		rootDev.setSim(true);
-		WorkerDeviceProcessor p = rootDev.getNewProcessor();
-		JobLoader l = new JobLoader((SimDeviceProcessor) p);
+		final WorkerDeviceProcessor p = rootDev.getNewProcessor();
+		new JobLoader((SimDeviceProcessor) p);
 
 	}
 
@@ -99,11 +99,13 @@ public class JobLoaderTest extends BambiTestCaseBase
 	{
 		final BambiTestDevice rootDev = new BambiTestDevice();
 		rootDev.setSim(true);
-		WorkerDeviceProcessor p = rootDev.getNewProcessor();
-		JobLoader l = new JobLoader((SimDeviceProcessor) p);
-		File destDir = new File(sm_dirTestDataTemp, "config");
+		final WorkerDeviceProcessor p = rootDev.getNewProcessor();
+		final JobLoader l = new JobLoader((SimDeviceProcessor) p);
+		final File destDir = rootDev.getCachedConfigDir();
 		if (!destDir.exists())
+		{
 			FileUtils.copyDirectory(new File(sm_dirTestData, "config"), destDir);
+		}
 		assertEquals(4, l.loadJob().size());
 
 	}
